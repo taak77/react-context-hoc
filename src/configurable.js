eede5d1cc8d095103028709e88082da37daf0767
Takashi Aoki <taak77@gmail.com>
2015-11-14 16:07:33 -0800

    added ESDoc

--- src/configurable.js
+++ src/configurable.js
@@ -1,13 +1,25 @@
 import React, {PropTypes, Component} from "react";
 import hoistStatics from "hoist-non-react-statics";
-
+/**
+ * @ignore
+ */
 function getDisplayName(Comp) {
     return Comp.displayName || Comp.name || "Component";
 }
-
-export default function configurable(context = {}, contextTypes = {}) {
-    const childContextTypes = Object.keys(context).reduce((types, key) => {
-        types[key] = contextTypes[key] || PropTypes.any;
+/**
+ * A component decorator to set props. Also available as context.
+ *
+ * @param {Object} [props={}] An object to set as props and context.
+ * @param {Object} [propTypes={}] A corresponding `propTypes` for the props object.
+ * @returns {Function}
+ * @public
+ */
+export default function configurable(props = {}, propTypes = {}) {
+    /**
+     * @ignore
+     */
+    const childContextTypes = Object.keys(props).reduce((types, key) => {
+        types[key] = propTypes[key] || PropTypes.any;
         return types;
     }, {});
 
@@ -18,12 +30,12 @@ export default function configurable(context = {}, contextTypes = {}) {
             static childContextTypes = childContextTypes
 
             getChildContext() {
-                return context;
+                return props;
             }
 
             render() {
                 return (
-                    <ComposedComponent {...this.props} {...context}/>
+                    <ComposedComponent {...this.props} {...props}/>
                 );
             }
         }
