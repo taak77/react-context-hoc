7bba665ee4b72fc7f90cb9be951a7b2769b775e0
Takashi Aoki <taak77@gmail.com>
2015-11-15 19:04:41 -0800

    added node version logging

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -11,6 +11,8 @@ module.exports = function (config) {
         singleRun: true
     });
 
+    console.log("TRAVIS_NODE_VERSION", process.env.TRAVIS_NODE_VERSION);
+
     if (process.env.USE_CLOUD === "true" && process.env.SAUCE_USERNAME) {
         require("./karma.conf.cloud.js")(config);
     } else {
